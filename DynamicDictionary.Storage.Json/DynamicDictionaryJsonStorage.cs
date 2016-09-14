a212edaef95e410c651cf97d6768c33d1b09e451
David Pires <davidblkx@outlook.com>
2016-09-15 00:01:06 +0100

    # Storage.Json - Added encoding support - Fix asynchronous performance

--- DynamicDictionary.Storage.Json/DynamicDictionaryJsonStorage.cs
+++ DynamicDictionary.Storage.Json/DynamicDictionaryJsonStorage.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using System;
+using System.Text;
 
 namespace Dynamic.Storage.Json
 {
@@ -14,49 +16,67 @@ namespace Dynamic.Storage.Json
 
         public string FilePath { get; set; }
         public Formatting JsonFormat { get; set; } = Formatting.None;
+        public Encoding EncondingFormat { get; set; } = Encoding.UTF8;
 
         public DynamicDictionary Load()
         {
-            DynamicDictionary dictionary = new DynamicDictionary();
-            var jsonString = File.ReadAllText(FilePath);
+            var jsonString = File.ReadAllText(FilePath, EncondingFormat);
 
+            DynamicDictionary dictionary = Deserialize(jsonString);
+
+            return dictionary;
+        }
+        public async Task<DynamicDictionary> LoadAsync()
+        {
+            string jsonString;
+
+            using(Stream reader = File.OpenRead(FilePath))
+            {
+                byte[] bytes = new byte[reader.Length];
+                await reader.ReadAsync(bytes, 0, bytes.Length);
+                jsonString = EncondingFormat.GetString(bytes);
+            }
+
+            return Deserialize(jsonString);
+        }
+
+        public bool Save(DynamicDictionary dictionary, DynamicDictionarySaveMotive motive)
+        {
+            var jsonString = Serialize(dictionary, JsonFormat);
+            File.WriteAllText(FilePath, jsonString, EncondingFormat);
+            return true;
+        }
+        public async Task<bool> SaveAsync(DynamicDictionary dictionary, DynamicDictionarySaveMotive motive)
+        {
+            var jsonString = Serialize(dictionary, JsonFormat);
+            using(FileStream writer = File.OpenWrite(FilePath))
+            {
+                byte[] bytes = EncondingFormat.GetBytes(jsonString);
+                await writer.WriteAsync(bytes, 0, bytes.Length);
+            }
+            return true;
+        }
+
+        public static string Serialize(DynamicDictionary dictionary, Formatting jsonFormat)
+        {
+            var serializable = new Dictionary<string, DynamicListValueSerializable>();
+            foreach (var i in dictionary.ToDictionary())
+            {
+                serializable.Add(i.Key, new DynamicListValueSerializable(i.Value));
+            }
+
+            return JsonConvert.SerializeObject(serializable, jsonFormat);
+        }
+        public static DynamicDictionary Deserialize(string jsonString)
+        {
+            DynamicDictionary dictionary = new DynamicDictionary();
             var serializable = JsonConvert.DeserializeObject<Dictionary<string, DynamicListValueSerializable>>(jsonString);
-            foreach(var v in serializable)
+            foreach (var v in serializable)
             {
                 dictionary[v.Key] = v.Value.Items;
             }
 
             return dictionary;
-        }
-
-        public async Task<DynamicDictionary> LoadAsync()
-        {
-            return await Task.Run<DynamicDictionary>(() =>
-            {
-                return Load();
-            });
-        }
-
-        public bool Save(DynamicDictionary dictionary, DynamicDictionarySaveMotive motive)
-        {
-            var serializable = new Dictionary<string, DynamicListValueSerializable>();
-            foreach(var i in dictionary.ToDictionary())
-            {
-                serializable.Add(i.Key, new DynamicListValueSerializable(i.Value));
-            }
-
-            var jsonString = JsonConvert.SerializeObject(serializable, JsonFormat);
-            File.WriteAllText(FilePath, jsonString);
-            return true;
-        }
-
-        public async Task<bool> SaveAsync(DynamicDictionary dictionary, DynamicDictionarySaveMotive motive)
-        {
-            await Task.Run(() =>
-            {
-                Save(dictionary, motive);
-            });
-            return true;
         }
     }
 }
